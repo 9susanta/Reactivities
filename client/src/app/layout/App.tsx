5e1860e26083f212099f7cce8116eee8c203de7a
9susanta <rsusanta0@gmail.com>
2025-05-30 22:20:09 +0530

    Basic UI Done with All Layour with API Date

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Container, CssBaseline, List, ListItem, ListItemText } from "@mui/material";
+import { Box, Container, CssBaseline } from "@mui/material";
 import axios from 'axios';
 import NavBar from './NavBar';
 import ActivityDashbord from '../../features/activities/dashboard/ActivityDashbord';
@@ -9,19 +9,19 @@ function App() {
 
   useEffect(()=>{
      axios.get<Activity>("https://localhost:5001/api/Activities")
-     .then(response=>setActivity(response.data));
+     .then(response=>setActivity(response.data as unknown as Activity[]));
 
      return ()=>{ console.log("In useEffect clean up")} //clean the useEffect
 
   },[])
   return (
-    <>
+    <Box sx={{background:"#eeeeee"}}>
       <CssBaseline/>
        <NavBar/>
        <Container maxWidth="xl" sx={{mt:3}}>
          <ActivityDashbord activities={activities}/>
        </Container>
-    </>
+    </Box>
   )
 }
 

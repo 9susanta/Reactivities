bba4344cd80dcfe351e641b4353e3ae81fb1c427
9susanta <rsusanta0@gmail.com>
2025-06-13 09:26:16 +0530

    Paging and Filter completed in API

---
+++ Application/Activities/Queries/ActivityParams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Activities.Queries
+{
+    public class ActivityParams : PaginationParams<DateTime?>
+    {
+        public string? Filter { get; set; }
+        public DateTime StartDate { get; set; } = DateTime.UtcNow;
+    }
+}
